81a28fd42f3aca0cf6f919799584a2d16fc84584
wenmeichao <2442008946@qq.com>
2021-02-10 16:01:21 +0800

    29

---
+++ drag.js
@@ -0,0 +1,34 @@
+function Drag(id) {
+    var _this = this
+    this.disX = 0
+    this.disY = 0
+    this.oDiv = document.getElementById(id)
+    this.oDiv.onmousedown = function () {
+        _this.fnDown()
+        return false
+    }
+}
+
+Drag.prototype.fnDown = function (ev) {
+    var oEvent = ev || event
+    var _this = this
+    this.disX = oEvent.clientX - this.oDiv.offsetLeft
+    this.disY = oEvent.clientY - this.oDiv.offsetTop
+    document.onmousemove = function (ev) {
+        _this.fnMove(ev)
+    }
+    document.onmouseup = function () {
+        _this.fnUp()
+    }
+}
+
+Drag.prototype.fnMove = function (ev) {
+    var oEvent = event || ev
+    this.oDiv.style.left = oEvent.clientX - this.disX + 'px'
+    this.oDiv.style.top = oEvent.clientY - this.disY + 'px'
+}
+
+Drag.prototype.fnUp = function () {
+    document.onmousemove = null
+    document.onmousedown = null
+}
